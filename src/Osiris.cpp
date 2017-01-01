5792a2e4c4536ef3bae7573d2078cabdfd34ccca
Daniel Loman <dloman@toyon.com>
2016-12-31 19:09:20 -0800

    added some cv stuff

--- src/Osiris.cpp
+++ src/Osiris.cpp
@@ -13,12 +13,6 @@ std::atomic<bool> gIsRunning(true);
 using namespace std::literals;
 
 //-----------------------------------------------------------------------------
-//-----------------------------------------------------------------------------
-void Interrupt(int)
-{
-  gIsRunning = false;
-}
-//------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
 int main(int argc, const char** argv)
 {
@@ -46,27 +40,51 @@ int main(int argc, const char** argv)
   {
     VideoSource >> VideoFrame;
 
-    std::vector<int> CompressionParameters{cv::IMWRITE_PNG_COMPRESSION, 9};
+    cv::Mat GreyFrame;
+    cv::cvtColor(VideoFrame, GreyFrame, cv::COLOR_BGR2GRAY);
 
-    std::time_t Time = std::time(nullptr);
-    std::tm LocalTime  = *std::localtime(&Time);
+    cv::Mat CannyFrame;
 
-    std::stringstream OutputFilename;
+    cv::Canny(GreyFrame, CannyFrame, 100, 200, 3);
 
-    OutputFilename
-      << OutputDirectory
-      << std::put_time(&LocalTime, "%F-%H-%M-%S")
-      << ".png";
+    cv::imshow("canny", CannyFrame);
 
-    //cv::Mat RotationMatrix;
-    //cv::transpose(VideoFrame, RotationMatrix);
-    //cv::flip(RotationMatrix, VideoFrame,1);
-      //imwrite(
-      //OutputFilename.str(),
-      //VideoFrame,
-      //{cv::IMWRITE_PNG_COMPRESSION, 9});
+    std::vector<std::vector<cv::Point>> Contours;
+
+    cv::findContours(
+      CannyFrame,
+      Contours,
+      cv::RETR_TREE,
+      cv::CHAIN_APPROX_SIMPLE);
+
+    cv::Mat HoughFrame;
+    VideoFrame.copyTo(HoughFrame);
+
+    for (auto i = 0u; i < Contours.size(); ++i)
+    {
+      cv::drawContours(VideoFrame, Contours, i, {255, 60, 60}, 2);
+    }
 
     cv::imshow("fuck", VideoFrame);
+
+    std::vector<cv::Vec4i> Lines;
+
+    cv::HoughLinesP(CannyFrame, Lines, 1, CV_PI / 180, 50, 50, 10);
+
+    for (size_t i = 0; i < Lines.size(); i++)
+    {
+      cv::Vec4i l = Lines[i];
+      cv::line(
+        HoughFrame,
+        cv::Point(l[0], l[1]),
+        cv::Point(l[2], l[3]),
+        cv::Scalar(0, 0, 255),
+        3,
+        2);
+
+    }
+
+    cv::imshow("hough", HoughFrame);
     cv::waitKey(3);
     //std::this_thread::sleep_for(1s);
 
