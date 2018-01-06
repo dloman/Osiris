857be7fe620578f69682685f9136c4708639a6cb
Daniel Loman <dloman@toyon.com>
2018-01-06 12:01:23 -0800

    added videolib reader

--- src/Osiris.cpp
+++ src/Osiris.cpp
@@ -1,3 +1,5 @@
+#include <VideoLib/VideoPlayer/VideoPlayer.hpp>
+
 #include "FilteredCircularBuffer.hpp"
 #include <opencv2/imgproc.hpp>
 #include <opencv2/videoio.hpp>
@@ -19,6 +21,10 @@ std::array<std::vector<double>, 3> gCleanTable{{
   {.077694, .089892, .303642, .305637, .144795, .056087, .015366, .006887},
   {.071506, .048397, .057974, .283089, .421860, .097151, .016917, .003106},
   {.077833, .042354, .053092, .052188, .239345, .499132, .035480, .000575}}};
+
+std::deque<cv::Mat> gFrames;
+
+FilteredCircularBuffer<10> gBoundingBoxBuffer;
 
 //-----------------------------------------------------------------------------
 //------------------------------------------------------------------------------
@@ -155,148 +161,152 @@ cv::Mat GetImageChip(const cv::Mat Image, cv::RotatedRect BoundingBox)
 
 //------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
-int main(int argc, const char** argv)
+void ProcessImage(cv::Mat& VideoFrame)
 {
-  std::deque<cv::Mat> Frames;
+  cv::Rect RegionOfInterest(0, 0, 640, 450);
 
-  FilteredCircularBuffer<10> BoundingBoxBuffer;
+  VideoFrame = VideoFrame(RegionOfInterest);
 
-  cv::VideoCapture VideoSource;
+  cv::Mat ColorFiltered;
 
-  VideoSource.open("/home/dloman/HackerspaceVideoData/Table.mkv");
+  int RedMinimum = 128, GreenMinimum = 65, BlueMinimum = 26;
 
-  if(!VideoSource.isOpened())
+  int RedMaximum = 204, GreenMaximum = 192, BlueMaximum = 150;
+
+  cv::Mat HsvFrame;
+
+  cv::cvtColor(VideoFrame, HsvFrame, cv::COLOR_BGR2HSV);
+
+  cv::inRange(
+    VideoFrame,
+    cv::Scalar(BlueMinimum, GreenMinimum, RedMinimum),
+    cv::Scalar(BlueMaximum, GreenMaximum, RedMaximum),
+    ColorFiltered);
+
+  cv::Mat MaskedFrame;
+
+  cv::bitwise_and(VideoFrame, VideoFrame, MaskedFrame, ColorFiltered);
+
+  gFrames.push_back(MaskedFrame);
+
+  if (gFrames.size() > 30)
   {
-    std::cerr << "ERROR: unable to open camera";
-
-    return 1;
+    gFrames.pop_front();
   }
 
-  cv::Mat VideoFrame;
+  cv::Mat AveragedFrame = cv::Mat::zeros(MaskedFrame.rows, MaskedFrame.cols, CV_32FC3);
 
-  while(gIsRunning)
+  for (auto& Frame : gFrames)
   {
-    VideoSource >> VideoFrame;
+    cv::accumulate(Frame, AveragedFrame);
+  }
 
-    if (VideoFrame.rows > 0 && VideoFrame.cols > 0)
+  AveragedFrame = AveragedFrame / gFrames.size();
+
+  AveragedFrame.convertTo(AveragedFrame, CV_8U);
+
+  cv::Mat GreyFrame;
+
+  cv::cvtColor(AveragedFrame, GreyFrame, cv::COLOR_BGR2GRAY);
+
+  cv::Mat CannyFrame;
+
+  cv::Canny(GreyFrame, CannyFrame, 250, 500, 3);
+
+  std::vector<cv::Vec4i> Lines;
+  std::vector<cv::Point> LinePoints;
+
+  cv::HoughLinesP(CannyFrame, Lines, 1, CV_PI / 180, 10, 50, 10);
+
+  cv::Mat HoughFrame;
+  VideoFrame.copyTo(HoughFrame);
+
+  cv::Mat FilteredFrame;
+  VideoFrame.copyTo(FilteredFrame);
+
+  cv::Mat LabFrame;
+  VideoFrame.copyTo(LabFrame);
+  cv::cvtColor(AveragedFrame, LabFrame, cv::COLOR_BGR2Luv);
+
+  for (size_t i = 0; i < Lines.size(); i++)
+  {
+    cv::Vec4i Line = Lines[i];
+    cv::line(
+      HoughFrame,
+      cv::Point(Line[0], Line[1]),
+      cv::Point(Line[2], Line[3]),
+      cv::Scalar(0, 0, 255),
+      3,
+      2);
+
+    LinePoints.emplace_back(Line[0], Line[1]);
+    LinePoints.emplace_back(Line[2], Line[3]);
+  }
+
+  if (LinePoints.size())
+  {
+    auto Temp = cv::minAreaRect(LinePoints);
+    auto DidItWork = gBoundingBoxBuffer.push_back(Temp);
+
+    if (DidItWork)
     {
-
-      cv::Rect RegionOfInterest(0, 0, 640, 450);
-
-      VideoFrame = VideoFrame(RegionOfInterest);
-
-      cv::Mat ColorFiltered;
-
-      int RedMinimum = 128, GreenMinimum = 65, BlueMinimum = 26;
-
-      int RedMaximum = 204, GreenMaximum = 192, BlueMaximum = 150;
-
-      cv::Mat HsvFrame;
-
-      cv::cvtColor(VideoFrame, HsvFrame, cv::COLOR_BGR2HSV);
-
-      cv::inRange(
-        VideoFrame,
-        cv::Scalar(BlueMinimum, GreenMinimum, RedMinimum),
-        cv::Scalar(BlueMaximum, GreenMaximum, RedMaximum),
-        ColorFiltered);
-
-      cv::Mat MaskedFrame;
-
-      cv::bitwise_and(VideoFrame, VideoFrame, MaskedFrame, ColorFiltered);
-
-      Frames.push_back(MaskedFrame);
-
-      if (Frames.size() > 30)
-      {
-        Frames.pop_front();
-      }
-
-      cv::Mat AveragedFrame = cv::Mat::zeros(MaskedFrame.rows, MaskedFrame.cols, CV_32FC3);
-
-      for (auto& Frame : Frames)
-      {
-        cv::accumulate(Frame, AveragedFrame);
-      }
-
-      AveragedFrame = AveragedFrame / Frames.size();
-
-      AveragedFrame.convertTo(AveragedFrame, CV_8U);
-
-      cv::Mat GreyFrame;
-
-      cv::cvtColor(AveragedFrame, GreyFrame, cv::COLOR_BGR2GRAY);
-
-      cv::Mat CannyFrame;
-
-      cv::Canny(GreyFrame, CannyFrame, 250, 500, 3);
-
-      std::vector<cv::Vec4i> Lines;
-      std::vector<cv::Point> LinePoints;
-
-      cv::HoughLinesP(CannyFrame, Lines, 1, CV_PI / 180, 10, 50, 10);
-
-      cv::Mat HoughFrame;
-      VideoFrame.copyTo(HoughFrame);
-
-      cv::Mat FilteredFrame;
-      VideoFrame.copyTo(FilteredFrame);
-
-      cv::Mat LabFrame;
-      VideoFrame.copyTo(LabFrame);
-      cv::cvtColor(AveragedFrame, LabFrame, cv::COLOR_BGR2Luv);
-
-      for (size_t i = 0; i < Lines.size(); i++)
-      {
-        cv::Vec4i Line = Lines[i];
-        cv::line(
-          HoughFrame,
-          cv::Point(Line[0], Line[1]),
-          cv::Point(Line[2], Line[3]),
-          cv::Scalar(0, 0, 255),
-          3,
-          2);
-
-        LinePoints.emplace_back(Line[0], Line[1]);
-        LinePoints.emplace_back(Line[2], Line[3]);
-      }
-
-      if (LinePoints.size())
-      {
-        auto Temp = cv::minAreaRect(LinePoints);
-        auto DidItWork = BoundingBoxBuffer.push_back(Temp);
-
-        if (DidItWork)
-        {
-          DrawRoundedRectangle(Temp, HoughFrame, cv::Scalar(60, 255, 60));
-        }
-        else
-        {
-          DrawRoundedRectangle(Temp, HoughFrame, cv::Scalar(255, 255, 60));
-        }
-      }
-
-      auto CurrentTablePosition = BoundingBoxBuffer.GetValue();
-
-      DrawRoundedRectangle(CurrentTablePosition, FilteredFrame, cv::Scalar(255, 60, 60));
-
-      auto ColorHistogram = ComputeColorHistogram(
-        GetImageChip(VideoFrame, CurrentTablePosition));
-
-      std::cout << ComputeScore(ColorHistogram) << '\n';
-
-
-      //cv::imshow("averaged", AveragedFrame);
-      //cv::imshow("hough", HoughFrame);
-      cv::imshow("filtered Frame", FilteredFrame);
-      cv::waitKey(1);
-
+      DrawRoundedRectangle(Temp, HoughFrame, cv::Scalar(60, 255, 60));
     }
     else
     {
-      return 0;
-      VideoSource.open("/home/dloman/HackerspaceVideoData/Table.mkv");
+      DrawRoundedRectangle(Temp, HoughFrame, cv::Scalar(255, 255, 60));
     }
+  }
+
+  auto CurrentTablePosition = gBoundingBoxBuffer.GetValue();
+
+  DrawRoundedRectangle(CurrentTablePosition, FilteredFrame, cv::Scalar(255, 60, 60));
+
+  auto ColorHistogram = ComputeColorHistogram(
+    GetImageChip(VideoFrame, CurrentTablePosition));
+
+  std::cout << "Table Score = " << ComputeScore(ColorHistogram) << '\n';
+
+  cv::imshow("filtered Frame", FilteredFrame);
+
+  cv::waitKey(1);
+}
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+int main(int argc, const char** argv)
+{
+  if (argc !=2)
+  {
+    std::cerr << "Osiris FILENAME\n";
+    exit(1);
+  }
+
+  vl::VideoPlayer videoPlayer(argv[1]);
+
+  cv::Mat VideoFrame;
+
+  videoPlayer.GetSignalFrame().Connect(
+    [] (const std::shared_ptr<const vl::Frame>& pFrame)
+    {
+      std::cout << pFrame->GetTime() << std::endl;
+
+      const auto& pImage = pFrame->GetImage();
+
+      cv::Mat VideoFrame(
+          pImage->GetHeight(),
+          pImage->GetWidth(),
+          CV_8UC3,
+          pImage->GetData().get());
+
+      cv::cvtColor(VideoFrame, VideoFrame, CV_BGR2RGB);
+
+      ProcessImage(VideoFrame);
+    });
+
+  while(gIsRunning)
+  {
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
   }
 
   return 0;
